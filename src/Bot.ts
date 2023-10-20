0008b63dd92cb9aceb0a8267871397130909b2a0
mKpwnz <knauer.michael@gmx.net>
2023-10-21 00:20:45 +0200

    Added Cronjob for Society Finance Management

--- src/Bot.ts
+++ src/Bot.ts
@@ -7,6 +7,7 @@ import { ExpressApp } from '@web/ExpressApp'
 import { Client, Events, IntentsBitField } from 'discord.js'
 import { CronManager } from '@utils/CronManager'
 import { CronJob } from 'cron'
+import { CronJobs } from '@controller/CronJobs.controller'
 LogManager.configure()
 
 const token = process.env.NODE_ENV === 'production' ? process.env.DISCORD_TOKEN_PROD : process.env.DISCORD_TOKEN_DEV
@@ -27,13 +28,11 @@ const client = new Client({
 Cache.init()
 EventHandler.init(client)
 client.login(token)
-client.once(Events.ClientReady, () => {
+client.once(Events.ClientReady, async () => {
     new ExpressApp()
-    // CronManager.initCronManager({
-    //     'fraktionen.finance': new CronJob('*/10 * * * * *', () => {
-    //         LogManager.debug('Cronjob started: fraktionen.finance')
-    //     }),
-    // })
+    CronManager.initCronManager({
+        'fraktionen.finance': new CronJob('0 0 0/2 ? * * *', () => CronJobs.logSocietyFinance()),
+    })
 })
 
 export const BotClient = client
